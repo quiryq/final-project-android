f646e2d3ccfaf5f724571858dc39a5fbc0c9130b
Farabi777 <f_kuanysh@kbtu.kz>
2025-12-09 04:49:05 +0500

    retrofit and data structure
    Я реализовал ретрофит базовый. Так же написал базовую структуру данных, как они должны выглядит и как их мы будем дальнейшем хранить в БД

--- app/src/main/java/com/example/final_project_android/data/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/final_project_android/data/api/RetrofitInstance.kt
@@ -1,4 +1,32 @@
 package com.example.final_project_android.data.api
 
-class RetrofitInstance {
+
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+
+    private const val BASE_URL = "https://www.themealdb.com/api/json/v1/1/"
+
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+
+    val api: MealApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+            .create(MealApi::class.java)
+    }
 }
