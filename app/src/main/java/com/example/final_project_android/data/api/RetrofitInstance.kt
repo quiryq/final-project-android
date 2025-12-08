5083fb2b0ea6f8bcd96b79c186c72fdc9f0f0cac
quiryq <n_mazhitov@kbtu.kz>
2025-12-09 04:25:44 +0500

    MVVM Architecture
    I've created a framework for an MVVM architecture.

---
+++ app/src/main/java/com/example/final_project_android/data/api/RetrofitInstance.kt
@@ -0,0 +1,4 @@
+package com.example.final_project_android.data.api
+
+class RetrofitInstance {
+}
---
+++ app/src/main/java/com/example/final_project_android/data/repository/MealRepository.kt
@@ -0,0 +1,4 @@
+package com.example.final_project_android.data.repository
+
+class MealRepository {
+}
