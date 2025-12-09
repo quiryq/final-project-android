7c27741b8b3e34d88e4002e1dceb4962fda657b9
Farabi777 <f_kuanysh@kbtu.kz>
2025-12-09 05:13:23 +0500

    repository
    repository :)

--- app/src/main/java/com/example/final_project_android/data/repository/MealRepository.kt
+++ app/src/main/java/com/example/final_project_android/data/repository/MealRepository.kt
@@ -1,4 +1,28 @@
 package com.example.final_project_android.data.repository
 
-class MealRepository {
+
+import com.example.final_project_android.data.api.RetrofitInstance
+import com.example.final_project_android.data.db.MealDatabase
+import com.example.final_project_android.data.model.Meal
+
+class MealRepository(
+    private val db: MealDatabase
+) {
+
+    suspend fun searchMeals(searchQuery: String) =
+        RetrofitInstance.api.searchMeals(searchQuery)
+
+
+    suspend fun getRandomMeal() =
+        RetrofitInstance.api.getRandomMeal()
+
+
+
+    suspend fun upsert(meal: Meal) = db.getMealDao().upsert(meal)
+
+
+    suspend fun delete(meal: Meal) = db.getMealDao().deleteMeal(meal)
+
+
+    fun getSavedMeals() = db.getMealDao().getAllMeals()
 }
